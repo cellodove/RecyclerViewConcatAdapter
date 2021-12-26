44cf4e79c28d76f57ba704d16395c103f4e88b89
wntjd200@naver.com <wntjd200@naver.com>
2021-12-26 14:45:29 +0900

    modifying listAdapter

--- app/src/main/java/com/peter/recyclerviewconcatadapter/adapter/AddressAdapter.kt
+++ app/src/main/java/com/peter/recyclerviewconcatadapter/adapter/AddressAdapter.kt
@@ -2,29 +2,42 @@ package com.peter.recyclerviewconcatadapter.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.peter.recyclerviewconcatadapter.databinding.AddressItemBinding
 import com.peter.recyclerviewconcatadapter.model.Address
 
-class AddressAdapter(private val address: ArrayList<Address>):RecyclerView.Adapter<AddressAdapter.AddressViewHolder>(){
+class AddressAdapter(private val clickItem: (Address) -> Unit ):ListAdapter<Address, AddressAdapter.AddressViewHolder>(diffUtil){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AddressViewHolder {
         val binding = AddressItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
-        return AddressViewHolder(binding)
+        val addressViewHolder = AddressViewHolder(binding)
+        binding.root.setOnClickListener {
+            clickItem(getItem(addressViewHolder.layoutPosition))
+        }
+        return addressViewHolder
     }
 
     override fun onBindViewHolder(holder: AddressViewHolder, position: Int) {
-        holder.bind(address[position])
-    }
-
-    override fun getItemCount(): Int {
-        return  address.size
+        holder.bind(getItem(position))
     }
 
     inner class AddressViewHolder(private val binding: AddressItemBinding):RecyclerView.ViewHolder(binding.root){
         fun bind(address : Address){
             binding.addressName.text = address.addressName
             binding.addressNumber.text = address.addressNumber
+        }
+    }
+    companion object{
+        val diffUtil = object : DiffUtil.ItemCallback<Address>(){
+            override fun areItemsTheSame(oldItem: Address, newItem: Address): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(oldItem: Address, newItem: Address): Boolean {
+                return oldItem.addressName == newItem.addressName
+            }
         }
     }
 }
--- app/src/main/java/com/peter/recyclerviewconcatadapter/adapter/UserAdapter.kt
+++ app/src/main/java/com/peter/recyclerviewconcatadapter/adapter/UserAdapter.kt
@@ -2,29 +2,43 @@ package com.peter.recyclerviewconcatadapter.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.peter.recyclerviewconcatadapter.databinding.UserItemBinding
 import com.peter.recyclerviewconcatadapter.model.User
 
-class UserAdapter(private val users: ArrayList<User>):RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+class UserAdapter(private val itemClick: (User) -> Unit):ListAdapter<User,UserAdapter.UserViewHolder>(diffUtil){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
         val binding = UserItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return UserViewHolder(binding)
+        val userViewHolder = UserViewHolder(binding)
+        binding.apply {
+            root.setOnClickListener {
+                itemClick(getItem(userViewHolder.layoutPosition))
+            }
+        }
+        return userViewHolder
     }
 
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
-        holder.bind(users[position])
+        holder.bind(getItem(position))
     }
-
-    override fun getItemCount(): Int {
-        return users.size
-    }
-
     inner class UserViewHolder(private val binding: UserItemBinding) : RecyclerView.ViewHolder(binding.root){
         fun bind(user: User){
             binding.userName.text = user.name
             binding.userImage.setImageResource(user.image)
             binding.userAge.text = user.age
+        }
+    }
+    companion object{
+        val diffUtil = object  : DiffUtil.ItemCallback<User>(){
+            override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
+                return  oldItem == newItem
+            }
+
+            override fun areContentsTheSame(oldItem: User, newItem: User): Boolean {
+                return oldItem.name == newItem.name
+            }
         }
     }
 }
