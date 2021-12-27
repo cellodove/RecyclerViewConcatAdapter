a8609b4599558e5f27424f033b4d36ddf2b9a7e0
wntjd200@naver.com <wntjd200@naver.com>
2021-12-27 23:31:16 +0900

    finish

--- app/src/main/java/com/peter/recyclerviewconcatadapter/DataSource.kt
+++ app/src/main/java/com/peter/recyclerviewconcatadapter/DataSource.kt
@@ -7,13 +7,17 @@ import com.peter.recyclerviewconcatadapter.model.User
 object DataSource {
 
     fun getUser() = ArrayList<User>().apply {
-        add(User("홍길동",R.drawable.ic_launcher_background,"20"))
-        add(User("홍길동",R.drawable.ic_launcher_background,"20"))
-        add(User("홍길동",R.drawable.ic_launcher_background,"20"))
-        add(User("홍길동",R.drawable.ic_launcher_background,"20"))
-        add(User("홍길동",R.drawable.ic_launcher_background,"20"))
-        add(User("홍길동",R.drawable.ic_launcher_background,"20"))
-
+        add(User("홍길동",R.drawable.grandfather1_icon,"50"))
+        add(User("배추도사",R.drawable.grandfather2_icon,"70"))
+        add(User("깔끔이",R.drawable.dentist_icon,"25"))
+        add(User("홍길순",R.drawable.grandmother1_icon,"51"))
+        add(User("한여름",R.drawable.grandmother2_icon,"45"))
+        add(User("상남자",R.drawable.man1_icon,"21"))
+        add(User("호크아이",R.drawable.man2_icon,"30"))
+        add(User("스타크",R.drawable.scientist_icon,"35"))
+        add(User("블랙위도우",R.drawable.woman1_icon,"29"))
+        add(User("이세경",R.drawable.woman2_icon,"21"))
+        add(User("백세나",R.drawable.woman3_icon,"28"))
     }
 
     fun getDepartment() = ArrayList<Department>().apply {
@@ -30,12 +34,16 @@ object DataSource {
 
     fun getAddress() = ArrayList<Address>().apply {
         add(Address("학과사무실","010-1234-5678"))
-        add(Address("학과사무실","010-1234-5678"))
-        add(Address("학과사무실","010-1234-5678"))
-        add(Address("학과사무실","010-1234-5678"))
-        add(Address("학과사무실","010-1234-5678"))
-        add(Address("학과사무실","010-1234-5678"))
-        add(Address("학과사무실","010-1234-5678"))
+        add(Address("홍길동 연구실","010-4568-5678"))
+        add(Address("상황실","010-7514-4984"))
+        add(Address("방재실","010-1238-9432"))
+        add(Address("위원회","010-1646-5678"))
+        add(Address("대학원","010-7945-8471"))
+        add(Address("총장실","010-9354-1205"))
+        add(Address("부총장실","010-7802-7912"))
+        add(Address("이사장","010-9843-0135"))
+        add(Address("방송부","010-5842-9145"))
+
 
     }
 }
